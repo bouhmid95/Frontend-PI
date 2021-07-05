7cdfaf75c5b48b9725aafbe1e8311966efa23da0
aymenJeddey <aymen.jeddey@esprit.tn>
2021-07-05 23:02:01 +0100

    user

--- Frontend-PI/Controllers/UserController.cs
+++ Frontend-PI/Controllers/UserController.cs
@@ -11,46 +11,109 @@ namespace Frontend_PI.Controllers
 {
     public class UserController : Controller
     {
-
-        // GET: User
-        public ActionResult Index() 
-        {
-            var tokenResponse = httpClient.GetAsync(baseAddress + " products ").Result;
-            if (tokenResponse.IsSuccessStatusCode)
-            {
-                var products = tokenResponse.Content.ReadAsAsync<IEnumerable<User>>().Result;
-                return View(products);
-            }
-            else
-            {
-                return View(new List<User>());
-            }
-        }
         HttpClient httpClient;
         string baseAddress;
-
-
         public UserController()
         {
-            baseAddress = " https://8081/SpringMVC/servlet/";
+            baseAddress = "http://localhost:8081/SpringMVC/servlet/";
             httpClient = new HttpClient();
             httpClient.BaseAddress = new Uri(baseAddress);
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(" application / json "));
-            var _AccessToken = Session[" AccessToken "];
-            httpClient.DefaultRequestHeaders.Add(" Authorization ", String.Format(" Bearer {0} ", _AccessToken));
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));//ACCEPT header
+            //var _AccessToken = Session[" AccessToken "];
+           // httpClient.DefaultRequestHeaders.Add(" Authorization ", String.Format(" Bearer {0} ", _AccessToken));
+        }
+        // GET: User
+        public ActionResult Index()
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://localhost:8081");
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage responseMessage = httpClient.GetAsync("SpringMVC/servlet/findUser/4").Result;
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                ViewBag.result = responseMessage.Content.ReadAsAsync<User>().Result;
+                return View(ViewBag.result);
+            }
+            return View();
         }
 
+        // GET: User/Details/5
+        public ActionResult Details()
+        {
 
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://localhost:8081");
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage responseMessage = httpClient.GetAsync("SpringMVC/servlet/findUser/4").Result;
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                ViewBag.result = responseMessage.Content.ReadAsAsync<User>().Result;
+                return View(ViewBag.result);
+            }
+            return View();
 
+        }
 
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            return View();
+
+        }
+
+        // POST: User/Create
         [HttpPost]
-        public ActionResult addUser(User user)
+        public ActionResult Create(User user)
         {
             try
             {
-                var APIResponse = httpClient.PostAsJsonAsync<User>(baseAddress + " addUser /",
+                var APIResponse = httpClient.PostAsJsonAsync<User>(baseAddress + "addUser/",
                 user).ContinueWith(postTask => postTask.Result.EnsureSuccessStatusCode());
-                return RedirectToAction(" Index ");
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
             }
             catch
             {
