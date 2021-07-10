7fd85077863732d91088cb44d194b91aa3acc61d
Ahmed Karoui <karouii.ahmed@gmail.com>
2021-07-10 14:34:50 +0100

    added list publication

---
+++ Frontend-PI/Controllers/PublicationController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Frontend_PI.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Frontend_PI.Controllers
+{
+    public class PublicationController : Controller
+
+
+    {
+
+        HttpClient httpClient;
+        string baseAddress;
+        public PublicationController()
+        {
+            baseAddress = "http://localhost:8081/SpringMVC/servlet/";
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(baseAddress);
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));//ACCEPT header
+                                                                                                                 //var _AccessToken = Session[" AccessToken "];
+                                                                                                                 // httpClient.DefaultRequestHeaders.Add(" Authorization ", String.Format(" Bearer {0} ", _AccessToken));
+        }
+        // GET: Publication
+        public ActionResult Index()
+        {
+            var APIResponse = httpClient.GetAsync(baseAddress + "findAllpublications");
+
+            //HttpResponseMessage responseMessage = httpClient.GetAsync("findUser/6").Result;
+
+            if (APIResponse.Result.IsSuccessStatusCode)
+            {
+                ViewBag.result = APIResponse.Result.Content.ReadAsAsync<IEnumerable<Publication>>().Result;
+                return View(ViewBag.result);
+            }
+            return View();
+        }
+
+        // GET: Publication/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Publication/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Publication/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Publication/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Publication/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Publication/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Publication/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Frontend-PI/Models/Publication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Frontend_PI.Models
+{
+    public class Publication
+    {
+        public int id { get; set; }
+        public string content { get; set; }
+        public DateTime publicationDate { get; set; }
+        public int nbLike { get; set; }
+        public int nbDisLike { get; set; }
+        public bool validated { get; set; }
+        public string image { get; set; }
+        public User user { get; set; }
+
+    }
+}
